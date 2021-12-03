b20ad6406ea2901fbf1a88629754d896dbf5d67e
ByMartrixx <bymartrixx@gmail.com>
2021-12-03 13:51:35 -0300

    1.18.1-pre1

--- buildSrc/src/main/java/quilt/internal/Constants.java
+++ buildSrc/src/main/java/quilt/internal/Constants.java
@@ -1,7 +1,7 @@
 package quilt.internal;
 
 public class Constants {
-    public static final String MINECRAFT_VERSION = "1.18";
+    public static final String MINECRAFT_VERSION = "1.18.1-pre1";
 
     public static final String MAPPINGS_NAME = "quilt-mappings";
 
