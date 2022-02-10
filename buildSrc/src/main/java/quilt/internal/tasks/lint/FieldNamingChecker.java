c2fa70ae2627ad4f684c3278626c3483593f0323
Eli Orona <eliorona@live.com>
2022-02-09 17:37:23 -0800

    Fix some issues

--- buildSrc/src/main/java/quilt/internal/tasks/lint/FieldNamingChecker.java
+++ buildSrc/src/main/java/quilt/internal/tasks/lint/FieldNamingChecker.java
@@ -18,13 +18,17 @@ public final class FieldNamingChecker implements Checker<FieldEntry> {
 
         AccessFlags access = accessProvider.apply(entry);
 
+        if (access == null) {
+            throw new RuntimeException("Invalid mappings detected. Please run './gradlew dropInvalidMappings'.");
+        }
+
         TypeDescriptor descriptor = entry.getDesc();
         boolean isAtomic = descriptor.isType() && descriptor.getTypeEntry().getFullName().toLowerCase(Locale.ROOT).contains("atomic");
         if (isAtomic) {
             if (startsWithUppercase(mapping.targetName())) {
                 errorReporter.warning("atomic field starts with uppercase character '" + mapping.targetName().charAt(0) + "'");
             }
-        } else if (access != null && access.isStatic() && access.isFinal()) {
+        } else if (access.isStatic() && access.isFinal()) {
             if (!isConstantCase(mapping.targetName())) {
                 if (entry.getDesc().isArray()) {
                     errorReporter.warning("static final array field is not in CONSTANT_CASE");
