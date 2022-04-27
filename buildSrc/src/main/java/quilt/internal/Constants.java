faf554795f4be0f0ee9bd8c044298916f18b1563
Eli Orona <eliorona@live.com>
2022-04-27 09:40:45 -0700

    Update to 22w17a

--- buildSrc/src/main/java/quilt/internal/Constants.java
+++ buildSrc/src/main/java/quilt/internal/Constants.java
@@ -1,7 +1,7 @@
 package quilt.internal;
 
 public class Constants {
-    public static final String MINECRAFT_VERSION = "22w16b";
+    public static final String MINECRAFT_VERSION = "22w17a";
 
     public static final String MAPPINGS_NAME = "quilt-mappings";
 
