de5a09091e066553876799e3d292f3b3a6b43055
Eli Orona <eliorona@live.com>
2022-06-21 11:51:01 -0700

    Update to 1.19.1-pre1

--- buildSrc/src/main/java/quilt/internal/Constants.java
+++ buildSrc/src/main/java/quilt/internal/Constants.java
@@ -1,7 +1,7 @@
 package quilt.internal;
 
 public class Constants {
-    public static final String MINECRAFT_VERSION = "22w24a";
+    public static final String MINECRAFT_VERSION = "1.19.1-pre1";
 
     public static final String MAPPINGS_NAME = "quilt-mappings";
 
