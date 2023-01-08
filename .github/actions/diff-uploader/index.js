ea63f9443a5b8756435aaffe4bfae1b5b43ec5b9
Eli Orona <eliorona@live.com>
2023-01-07 21:00:17 -0800

    Maybe comments?

---
+++ .github/actions/diff-uploader/index.js
@@ -0,0 +1,59 @@
+const core = require('@actions/core');
+const {context, getOctokit} = require('@actions/github');
+const fs = require("fs/promises");
+
+async function main() {
+    const token = core.getInput('github-token', {required: true});
+    const octokit = getOctokit(token);
+    const github = octokit.rest;
+
+    const repo = context.repo.repo;
+    const owner = context.repo.owner;
+    const baseParameters = {owner, repo};
+
+    let comment = async function (comment) {
+        return console.log(comment);
+    }
+
+    if (context.eventName === "push") {
+        comment = async function (comment) {
+            return github.repos.createCommitComment({
+                ...baseParameters,
+                commit_sha: context.sha,
+                body: comment
+            });
+        }
+    } else if (context.eventName === "pull_request") {
+        comment = async function (comment) {
+            return github.issues.createComment({
+                ...baseParameters,
+                issue_number: context.event.number,
+                body: comment
+            });
+        }
+    }
+
+    try {
+        const diff_buffer = await fs.readFile("../../../target.diff");
+        const diff = diff_buffer.toString();
+
+        if (diff === "") {
+            await comment("No difference between head and target.");
+            core.setOutput("result", "skip");
+        } else {
+            let changed_lines = (diff.match(new RegExp("^\\+[^+]", "gm")) || []).length;
+            let changed_files = (diff.match(new RegExp("^\\+\\+\\+", "gm")) || []).length;
+
+            core.setOutput("result", "success");
+            await comment(`With commit ${context.sha}, ${changed_files} file(s) where updated and ${changed_lines} line(s) were modified. \n\n\<details\>\n\<summary\>View the diff here:\</summary\> \n\<br\>\n\n\`\`\`diff\n${diff}\`\`\`\n\</details\>`)
+        }
+    } catch (err) {
+        await comment(`No diff file generated:\nError ${err}`);
+        core.setOutput("result", "failed");
+    }
+}
+
+main().catch(error => {
+    core.setFailed(error.message);
+    core.error(error.stack);
+});
