bdac65877953f33a8745bbaa5e7d315e1957349f
supersaiyansubtlety <supersaiyansubtlety@gmail.com>
2024-07-23 03:44:48 -0700

    check for empty, non-mapping content, and wrong extension files; check non-obfuscated classes; log similarly to MappingLintTask with a short exception message

--- buildSrc/src/main/java/quilt/internal/tasks/lint/FindDuplicateMappingFilesTask.java
+++ buildSrc/src/main/java/quilt/internal/tasks/lint/FindDuplicateMappingFilesTask.java
@@ -5,14 +5,14 @@ import com.google.common.collect.Multimap;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.GradleException;
 import org.gradle.api.file.DirectoryProperty;
+import org.gradle.api.logging.Logger;
+import org.gradle.api.logging.Logging;
 import org.gradle.api.tasks.InputDirectory;
 import org.gradle.api.tasks.TaskAction;
 
 import java.io.*;
 import java.nio.file.Files;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 import java.util.regex.Pattern;
 import java.nio.file.Path;
 import java.util.stream.Stream;
@@ -20,7 +20,9 @@ import java.util.stream.Stream;
 public abstract class FindDuplicateMappingFilesTask extends DefaultTask {
     public static final String TASK_NAME = "findDuplicateMappingFiles";
 
-    private static final Pattern CLASS_HASH_PATTERN = Pattern.compile("^CLASS net/minecraft/unmapped/C_\\w+");
+    private static final Logger LOGGER = Logging.getLogger(FindDuplicateMappingFilesTask.class);
+    private static final Pattern MINECRAFT_CLASS = Pattern.compile("^CLASS net/minecraft/(?:\\w+/)*\\w+ ");
+    private static final Pattern BLAZE_CLASS = Pattern.compile("^CLASS com/mojang/blaze3d/(?:\\w+/)*\\w+ ");
 
     @InputDirectory
     public abstract DirectoryProperty getMappingDirectory();
@@ -28,7 +30,10 @@ public abstract class FindDuplicateMappingFilesTask extends DefaultTask {
     @TaskAction
     public void run() {
         final Multimap<String, File> allMappings = HashMultimap.create();
-        final Set<String> multiMappings = new HashSet<>();
+        final Set<String> duplicateMappings = new HashSet<>();
+        final List<File> emptyFiles = new ArrayList<>();
+        final List<File> mapNoClassFiles = new ArrayList<>();
+        final List<File> wrongExtensionFiles = new ArrayList<>();
 
         try (Stream<Path> mappingPaths = Files.walk(getMappingDirectory().get().getAsFile().toPath())) {
             mappingPaths.map(Path::toFile)
@@ -37,15 +42,22 @@ public abstract class FindDuplicateMappingFilesTask extends DefaultTask {
                     try (var reader = new BufferedReader(new FileReader(mappingFile))) {
                         final String firstLine = reader.readLine();
                         if (firstLine != null) {
-                            final var classHashMatcher = CLASS_HASH_PATTERN.matcher(firstLine);
-                            if (classHashMatcher.find()) {
-                                final String classHash = classHashMatcher.group(0);
-                                final Collection<File> classMappings = allMappings.get(classHash);
+                            getClassMatch(firstLine).ifPresentOrElse(
+                                classMatch -> {
+                                    final Collection<File> classMappings = allMappings.get(classMatch);
 
-                                if (!classMappings.isEmpty()) multiMappings.add(classHash);
+                                    if (!classMappings.isEmpty()) duplicateMappings.add(classMatch);
 
-                                classMappings.add(mappingFile);
+                                    classMappings.add(mappingFile);
+                                },
+                                () -> mapNoClassFiles.add(mappingFile)
+                            );
+
+                            if (!mappingFile.toString().endsWith(".mapping")) {
+                                wrongExtensionFiles.add(mappingFile);
                             }
+                        } else {
+                            emptyFiles.add(mappingFile);
                         }
                     } catch (IOException e) {
                         throw new GradleException("Unexpected error accessing mapping file", e);
@@ -55,20 +67,95 @@ public abstract class FindDuplicateMappingFilesTask extends DefaultTask {
             throw new GradleException("Unexpected error accessing mappings directory", e);
         }
 
-        if (!multiMappings.isEmpty()) {
-            final var err = new StringBuilder("Found classes mapped by multiple files!");
-            err.append(System.lineSeparator());
-            for (final String multiMapping : multiMappings) {
-                err.append('\t').append(multiMapping).append(" is mapped by:");
-                err.append(System.lineSeparator());
+        final List<String> errorMessages = new ArrayList<>();
+        if (!duplicateMappings.isEmpty()) {
+            final String message = "%d class%s mapped by multiple files".formatted(
+                duplicateMappings.size(),
+                duplicateMappings.size() == 1 ? "" : "es"
+            );
+            errorMessages.add(message);
 
-                for (final File mappingFile : allMappings.get(multiMapping)) {
-                    err.append("\t\t").append(mappingFile);
-                    err.append(System.lineSeparator());
+            LOGGER.error("Found {}!", message);
+            for (final String duplicateMapping : duplicateMappings) {
+                LOGGER.error("\t{} is mapped by:", duplicateMapping);
+
+                for (final File mappingFile : allMappings.get(duplicateMapping)) {
+                    LOGGER.error("\t\t{}", mappingFile);
+                }
+            }
+        }
+
+        if (!emptyFiles.isEmpty()) {
+            final String message = "%d empty file%s".formatted(
+                emptyFiles.size(),
+                emptyFiles.size() == 1 ? "" : "s"
+            );
+            errorMessages.add(message);
+
+            LOGGER.error("Found {}!", message);
+            for (final File emptyFile : emptyFiles) {
+                LOGGER.error("\t{}", emptyFile);
+            }
+        }
+
+        if (!mapNoClassFiles.isEmpty()) {
+            final String message = "%d file%s not mapping a class".formatted(
+                mapNoClassFiles.size(),
+                mapNoClassFiles.size() == 1 ? "" : "s"
+            );
+            errorMessages.add(message);
+
+            LOGGER.error("Found {}!", message);
+            for (final File mapNoClassFile : mapNoClassFiles) {
+                LOGGER.error("\t{}", mapNoClassFile);
+            }
+        }
+
+        if (!wrongExtensionFiles.isEmpty()) {
+            final String message = "%d file%s without the mapping extension".formatted(
+                wrongExtensionFiles.size(),
+                wrongExtensionFiles.size() == 1 ? "" : "s"
+            );
+            errorMessages.add(message);
+
+            LOGGER.error("Found {}!", message);
+            for (final File mapNoClassFile : mapNoClassFiles) {
+                LOGGER.error("\t{}", mapNoClassFile);
+            }
+        }
+
+        if (!errorMessages.isEmpty()) {
+            final var fullError = new StringBuilder("Found ");
+            switch (errorMessages.size()) {
+                case 1 -> { }
+                case 2 -> fullError.append(errorMessages.getFirst()).append(" and ");
+                default -> {
+                    final List<String> allButLastMessage = errorMessages.subList(0, errorMessages.size() - 1);
+                    for (final String message : allButLastMessage) {
+                        fullError.append(message).append(", ");
+                    }
+
+                    fullError.append("and ");
                 }
             }
 
-            throw new GradleException(err.toString());
+            fullError.append(errorMessages.getLast()).append("! See the log for details.");
+
+            throw new GradleException(fullError.toString());
+        }
+    }
+
+    private static Optional<String> getClassMatch(String firstLine) {
+        final var minecraftClassMatcher = MINECRAFT_CLASS.matcher(firstLine);
+        if (minecraftClassMatcher.find()) {
+            return Optional.of(minecraftClassMatcher.group(0));
+        } else {
+            final var blazeClassMatcher = BLAZE_CLASS.matcher(firstLine);
+            if (blazeClassMatcher.find()) {
+                return Optional.of(blazeClassMatcher.group(0));
+            } else {
+                return Optional.empty();
+            }
         }
     }
 }
