a6f8b98399e51c5d8911c9802c4c7ca1eb34c488
supersaiyansubtlety <supersaiyansubtlety@gmail.com>
2024-07-23 00:02:20 -0700

    create DuplicateMappingFinderTask

---
+++ buildSrc/src/main/java/quilt/internal/tasks/lint/DuplicateMappingFinderTask.java
@@ -0,0 +1,67 @@
+package quilt.internal.tasks.lint;
+
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+import org.gradle.api.DefaultTask;
+import org.gradle.api.GradleException;
+import org.gradle.api.file.RegularFileProperty;
+import org.gradle.api.tasks.InputDirectory;
+import org.gradle.api.tasks.TaskAction;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Pattern;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+public abstract class DuplicateMappingFinderTask extends DefaultTask {
+    private static final Pattern CLASS_HASH_PATTERN = Pattern.compile("^CLASS net/minecraft/unmapped/C_\\w+");
+
+    @InputDirectory
+    public abstract RegularFileProperty getMappingsDir();
+
+    @TaskAction
+    public void run() {
+        final Multimap<String, File> allMappings = HashMultimap.create();
+        final Set<String> multiMappings = new HashSet<>();
+
+        try (Stream<Path> mappingPaths = Files.walk(getMappingsDir().get().getAsFile().toPath())) {
+            mappingPaths.map(Path::toFile)
+                .filter(File::isFile)
+                .forEach(mappingFile -> {
+                    try (var reader = new BufferedReader(new FileReader(mappingFile))) {
+                        final String firstLine = reader.readLine();
+                        if (firstLine != null) {
+                            final var classHashMatcher = CLASS_HASH_PATTERN.matcher(firstLine);
+                            if (classHashMatcher.find()) {
+                                final String classHash = classHashMatcher.group(0);
+                                final Collection<File> classMappings = allMappings.get(classHash);
+
+                                if (!classMappings.isEmpty()) multiMappings.add(classHash);
+
+                                classMappings.add(mappingFile);
+                            }
+                        }
+                    } catch (IOException e) {
+                        throw new GradleException("Unexpected error accessing mapping file", e);
+                    }
+                });
+        } catch (IOException e) {
+            throw new GradleException("Unexpected error accessing mappings directory", e);
+        }
+
+        if (!multiMappings.isEmpty()) {
+            final var err = new StringBuilder("Found classes mapped by multiple files!");
+            for (final String multiMapping : multiMappings) {
+                err.append('\t').append(multiMapping).append(" is mapped by:");
+
+                for (final File mappingFile : allMappings.get(multiMapping)) {
+                    err.append("\t\t").append(mappingFile);
+                }
+            }
+        }
+    }
+}
