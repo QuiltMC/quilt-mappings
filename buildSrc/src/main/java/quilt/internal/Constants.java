283e70c7f939f58f6b57ad12c2bbb901c902aa19
ix0rai <ix0rai64@gmail.com>
2024-09-11 11:55:26 -0500

    24w36a

--- buildSrc/src/main/java/quilt/internal/Constants.java
+++ buildSrc/src/main/java/quilt/internal/Constants.java
@@ -1,7 +1,7 @@
 package quilt.internal;
 
 public class Constants {
-    public static final String MINECRAFT_VERSION = "24w35a";
+    public static final String MINECRAFT_VERSION = "24w36a";
 
     public static final String MAPPINGS_NAME = "quilt-mappings";
 
