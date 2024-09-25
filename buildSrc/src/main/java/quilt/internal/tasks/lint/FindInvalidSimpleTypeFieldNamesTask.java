cd0714c3c9d171f4fecaf12b20800f33626450d2
supersaiyansubtlety <supersaiyansubtlety@gmail.com>
2024-09-25 00:58:58 -0700

    map ~97/270 RegsitryKeys affected by their removal from simple type names rename IdListUtil#sortArray -> createByIdGetter make enums that use createByIdGetter consistent restore SpawnReason add simple type names for Entity+LivinEntity add stub FindInvalidSimpleTypeFieldNamesTask class map a few other things along the way

---
+++ buildSrc/src/main/java/quilt/internal/tasks/lint/FindInvalidSimpleTypeFieldNamesTask.java
@@ -0,0 +1,11 @@
+package quilt.internal.tasks.lint;
+
+import org.gradle.api.DefaultTask;
+
+/*
+TODO
+ - implement
+ - see if hashed classes can come from enigma
+ */
+public abstract class FindInvalidSimpleTypeFieldNamesTask extends DefaultTask {
+}
