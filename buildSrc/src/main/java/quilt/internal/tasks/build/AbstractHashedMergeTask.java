397ff37992331dab7b6714967fa0905f2d2d55af
supersaiyansubtlety <supersaiyansubtlety@gmail.com>
2024-09-30 22:20:11 -0700

    move conventions for MergeTinyTask, MergeTinyV2Task, and MergeIntermediaryTask to MappingsPlugin refine their and their superclass' properties

--- buildSrc/src/main/java/quilt/internal/tasks/build/AbstractHashedMergeTask.java
+++ buildSrc/src/main/java/quilt/internal/tasks/build/AbstractHashedMergeTask.java
@@ -1,20 +1,19 @@
 package quilt.internal.tasks.build;
 
+import org.gradle.api.file.RegularFileProperty;
+import org.gradle.api.tasks.InputFile;
 import quilt.internal.Constants;
 
-import java.io.File;
-
 public abstract class AbstractHashedMergeTask extends AbstractTinyMergeTask {
-    public AbstractHashedMergeTask(String outputMappings) {
-        super(outputMappings, Constants.PER_VERSION_MAPPINGS_NAME);
+    @InputFile
+    public abstract RegularFileProperty getHashedTinyMappings();
+
+    public AbstractHashedMergeTask() {
+        super(Constants.PER_VERSION_MAPPINGS_NAME);
     }
 
     @Override
     public void mergeMappings() throws Exception {
-        final File hashedTinyInput =
-            this.getTaskNamed(InvertPerVersionMappingsTask.TASK_NAME, InvertPerVersionMappingsTask.class)
-                .getInvertedTinyFile().get().getAsFile();
-
-        this.mergeMappings(hashedTinyInput);
+        this.mergeMappings(this.getHashedTinyMappings().get().getAsFile());
     }
 }
