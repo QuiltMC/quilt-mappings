b48cd11593348c2766dd2a6159ee40a9fa7c9073
supersaiyansubtlety <supersaiyansubtlety@gmail.com>
2024-10-30 00:03:13 -0700

    remove DownloadMinecraftJarsTask's outsputs upToDateWhen checksum check: it was unecessary and caused outputs to never be up to date

--- buildSrc/src/main/java/quilt/internal/task/setup/DownloadMinecraftJarsTask.java
+++ buildSrc/src/main/java/quilt/internal/task/setup/DownloadMinecraftJarsTask.java
@@ -1,11 +1,5 @@
 package quilt.internal.task.setup;
 
-import java.io.File;
-import java.io.IOException;
-
-import com.google.common.hash.HashCode;
-import com.google.common.hash.Hashing;
-import com.google.common.io.Files;
 import org.gradle.api.GradleException;
 import org.gradle.api.file.RegularFileProperty;
 import org.gradle.api.tasks.OutputFile;
@@ -38,27 +32,6 @@ public abstract class DownloadMinecraftJarsTask extends DefaultMappingsTask impl
 
     public DownloadMinecraftJarsTask() {
         super(Groups.SETUP);
-
-        // TODO CACHE I'm not sure that this is necessary
-        //  VersionParserConsumingTasks indirectly depend on
-        //  DownloadVersionManifestFileTask which has @DisableCachingByDefault.
-        //  I'm not sure if gradle considers output files to have been updated when they're overwritten with
-        //  the same content.
-        //  If not, this can be eliminated.
-        //  If so, we should do a check like this in DownloadVersionManifestFileTask so checks like this are obsolete.
-        this.getOutputs().upToDateWhen(unused -> {
-            try {
-                final File clientJar = this.getClientJar().get().getAsFile();
-                final File serverBootstrapJar = this.getServerBootstrapJar().get().getAsFile();
-                final Downloads downloads = this.getVersionParser().get().get().getDownloads();
-
-                return clientJar.exists() && serverBootstrapJar.exists()
-                    && validateChecksum(clientJar, downloads.getClient().getSha1())
-                    && validateChecksum(serverBootstrapJar, getServerOrThrow(downloads).getSha1());
-            } catch (Exception e) {
-                return false;
-            }
-        });
     }
 
     @TaskAction
@@ -85,20 +58,5 @@ public abstract class DownloadMinecraftJarsTask extends DefaultMappingsTask impl
     private static DownloadableFile getServerOrThrow(Downloads downloads) {
         return downloads.getServer()
             .orElseThrow(() -> new GradleException("Version has no server download"));
-    }
-
-    @SuppressWarnings("deprecation")
-    private static boolean validateChecksum(File file, String checksum) throws IOException {
-        if (file != null) {
-            final HashCode hash = Files.asByteSource(file).hash(Hashing.sha1());
-            final StringBuilder builder = new StringBuilder();
-            for (final byte b : hash.asBytes()) {
-                builder.append(Integer.toString((b & 0xFF) + 0x100, 16).substring(1));
-            }
-
-            return builder.toString().equals(checksum);
-        }
-
-        return false;
     }
 }
