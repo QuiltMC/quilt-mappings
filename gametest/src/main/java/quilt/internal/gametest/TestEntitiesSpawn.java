54031691966755e24353f083cf8f938463886d38
supersaiyansubtlety <supersaiyansubtlety@gmail.com>
2025-07-11 13:47:59 -0700

    update gametest code for new mappings

--- gametest/src/main/java/quilt/internal/gametest/TestEntitiesSpawn.java
+++ gametest/src/main/java/quilt/internal/gametest/TestEntitiesSpawn.java
@@ -4,8 +4,8 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.test.AbstractTestFunction;
 import net.minecraft.test.TestContext;
-import net.minecraft.unmapped.C_pxdgchfu;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.GameMode;
@@ -13,8 +13,7 @@ import net.minecraft.world.GameMode;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
-// C_pxdgchfu is AbstractTestFunction
-public class TestEntitiesSpawn extends C_pxdgchfu {
+public class TestEntitiesSpawn extends AbstractTestFunction {
     public static final String PATH = "entities_spawn";
 
     public static void spawnEachEntity(TestContext context) {
@@ -27,9 +26,8 @@ public class TestEntitiesSpawn extends C_pxdgchfu {
         context.complete();
     }
 
-    // method_66916 is register
     @Override
-    public void method_66916(BiConsumer<RegistryKey<Consumer<TestContext>>, Consumer<TestContext>> registrar) {
+    public void register(BiConsumer<RegistryKey<Consumer<TestContext>>, Consumer<TestContext>> registrar) {
         final RegistryKey<Consumer<TestContext>> key =
             RegistryKey.of(RegistryKeys.TEST_FUNCTION, Identifier.of("quilt", PATH));
 
--- gametest/src/main/java/quilt/internal/gametest/mixin/TestFunctionMixin.java
+++ gametest/src/main/java/quilt/internal/gametest/mixin/TestFunctionMixin.java
@@ -3,9 +3,9 @@ package quilt.internal.gametest.mixin;
 import quilt.internal.gametest.TestEntitiesSpawn;
 
 import net.minecraft.registry.Registry;
+import net.minecraft.test.AbstractTestFunction;
 import net.minecraft.test.TestContext;
-import net.minecraft.unmapped.C_pxdgchfu;
-import net.minecraft.unmapped.C_zmxlezwg;
+import net.minecraft.test.TestFunction;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,19 +14,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.function.Consumer;
 
-// C_zmxlezwg is TestFunction
-@Mixin(C_zmxlezwg.class)
+@Mixin(TestFunction.class)
 class TestFunctionMixin {
-    // method_66917 is bootstrap
-    @Inject(method = "method_66917", at = @At(
+    @Inject(method = "bootstrap", at = @At(
         value = "INVOKE",
-        // C_zmxlezwg::method_67077 is AbstractTestFunction::bootstrap
-        target = "Lnet/minecraft/unmapped/C_zmxlezwg;method_67077(Lnet/minecraft/registry/Registry;)V"
+        target = "Lnet/minecraft/test/TestFunction;registerFunctions(Lnet/minecraft/registry/Registry;)V"
     ))
     private static void boostrapFunctions(
         Registry<Consumer<TestContext>> registry, CallbackInfoReturnable<Consumer<TestContext>> cir
     ) {
-        // C_pxdgchfu::method_67076 is AbstractTestFunction::register
-        C_pxdgchfu.method_67076(new TestEntitiesSpawn());
+        AbstractTestFunction.register(new TestEntitiesSpawn());
     }
 }
